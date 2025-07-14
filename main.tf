af1cdf1e5377a851b5beead4d34bd0a1af0ac250
Charalambos Theodorou <43213510+charalambos02@users.noreply.github.com>
2025-07-14 21:23:38 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "env0-test-instance"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "env0-test-bucket"
+}
