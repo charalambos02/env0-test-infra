6809b571e32db2710aab152ed4d1e25d2c94961c
Charalambos Theodorou <43213510+charalambos02@users.noreply.github.com>
2025-07-15 20:51:17 +0100

    env0 AI: PR Auto-Review changes

--- main.tf
+++ main.tf
@@ -1,15 +1,2 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "env0-test-instance"
-  }
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "env0-test-bucket"
-}
+### Suggested Enhanced Code:
